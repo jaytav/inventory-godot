077ed3fb85c216a27fe1a389b315c777dbb07af0
Jayden Tavares <jayden.tavares@hotmail.com>
2024-02-03 19:42:12 +1100

    add Items and ItemSlots properties to Inventory, add functionality in WorldInventory to create WorldItem and WorldItemSlot based on Inventory properties

--- src/inventory/Inventory.cs
+++ src/inventory/Inventory.cs
@@ -1,6 +1,11 @@
 using Godot;
+using Godot.Collections;
 
 public partial class Inventory : Resource
 {
+    [Export]
+    public Dictionary<Vector2, Item> Items;
 
+    [Export]
+    public Dictionary<Vector2, ItemSlot> ItemSlots;
 }
--- src/scenes/world/inventory/WorldInventory.cs
+++ src/scenes/world/inventory/WorldInventory.cs
@@ -4,4 +4,38 @@ public partial class WorldInventory : Node2D
 {
     [Export]
     public Inventory Inventory;
+
+    /**
+    * Scene nodes
+    */
+    private Node2D _items;
+    private Node2D _itemSlots;
+
+    private PackedScene _worldItem = GD.Load<PackedScene>("res://src/scenes/world/inventory/WorldItem.tscn");
+    private PackedScene _worldItemSlot = GD.Load<PackedScene>("res://src/scenes/world/inventory/WorldItemSlot.tscn");
+
+    public override void _Ready()
+    {
+        InitialiseSceneNodes();
+
+        foreach (Vector2 itemPosition in Inventory.Items.Keys)
+        {
+            WorldItem worldItem = _worldItem.Instantiate<WorldItem>();
+            worldItem.Position = itemPosition * 64;
+            _items.AddChild(worldItem);
+        }
+
+        foreach (Vector2 itemSlotPosition in Inventory.ItemSlots.Keys)
+        {
+            WorldItemSlot worldItemSlot = _worldItemSlot.Instantiate<WorldItemSlot>();
+            worldItemSlot.Position = itemSlotPosition * 64;
+            _itemSlots.AddChild(worldItemSlot);
+        }
+    }
+
+    private void InitialiseSceneNodes()
+    {
+        _items = GetNode<Node2D>("Items");
+        _itemSlots = GetNode<Node2D>("ItemSlots");
+    }
 }
