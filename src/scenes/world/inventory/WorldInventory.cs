b0ee1b8823d920381d0ac80b178b2a0729353660
Jayden Tavares <jayden.tavares@hotmail.com>
2024-02-03 20:59:28 +1100

    add WorldInventory handling hovering on WorldItem and WorldItemSlot, add highlighting for WorldItem when hovering

--- src/scenes/world/inventory/WorldInventory.cs
+++ src/scenes/world/inventory/WorldInventory.cs
@@ -1,18 +1,23 @@
 using Godot;
+using Godot.Collections;
 
 public partial class WorldInventory : Node2D
 {
     [Export]
     public Inventory Inventory;
 
+    private Array<WorldItem> _hoveredWorldItems = new();
+    private Array<WorldItemSlot> _hoveredWorldItemSlots = new();
+
+    private PackedScene _worldItem = GD.Load<PackedScene>("res://src/scenes/world/inventory/WorldItem.tscn");
+    private PackedScene _worldItemSlot = GD.Load<PackedScene>("res://src/scenes/world/inventory/WorldItemSlot.tscn");
+
     /**
     * Scene nodes
     */
     private Node2D _items;
     private Node2D _itemSlots;
-
-    private PackedScene _worldItem = GD.Load<PackedScene>("res://src/scenes/world/inventory/WorldItem.tscn");
-    private PackedScene _worldItemSlot = GD.Load<PackedScene>("res://src/scenes/world/inventory/WorldItemSlot.tscn");
+    private Area2D _mouseArea;
 
     public override void _Ready()
     {
@@ -33,9 +38,63 @@ public partial class WorldInventory : Node2D
         }
     }
 
+    public override void _Process(double delta)
+    {
+        _mouseArea.GlobalPosition = GetGlobalMousePosition();
+    }
+
     private void InitialiseSceneNodes()
     {
         _items = GetNode<Node2D>("Items");
         _itemSlots = GetNode<Node2D>("ItemSlots");
+        _mouseArea = GetNode<Area2D>("MouseArea");
+        _mouseArea.GlobalPosition = GetGlobalMousePosition();
+    }
+
+    /**
+    * Signal receivers
+    */
+    private void OnMouseAreaAreaEntered(Area2D area)
+    {
+        Node areaParent = area.GetParent();
+
+        if (areaParent is WorldItem)
+        {
+            foreach (WorldItem hoveredWorldItem in _hoveredWorldItems)
+            {
+                hoveredWorldItem.Unhighlight();
+            }
+
+            WorldItem worldItem = (WorldItem)areaParent;
+            _hoveredWorldItems.Insert(0, worldItem);
+            worldItem.Highlight();
+        }
+        else if (areaParent is WorldItemSlot)
+        {
+            WorldItemSlot worldItemSlot = (WorldItemSlot)areaParent;
+            _hoveredWorldItemSlots.Insert(0, worldItemSlot);
+        }
+    }
+
+    private void OnMouseAreaAreaExited(Area2D area)
+    {
+        Node areaParent = area.GetParent();
+
+        if (areaParent is WorldItem)
+        {
+            WorldItem worldItem = (WorldItem)areaParent;
+            _hoveredWorldItems.Remove(worldItem);
+            worldItem.Unhighlight();
+
+            if (_hoveredWorldItems.Count > 0)
+            {
+                _hoveredWorldItems[0].Highlight();
+            }
+        }
+        else if (areaParent is WorldItemSlot)
+        {
+            WorldItemSlot worldItemSlot = (WorldItemSlot)areaParent;
+            _hoveredWorldItemSlots.Remove(worldItemSlot);
+        }
     }
 }
--- src/scenes/world/inventory/WorldItem.cs
+++ src/scenes/world/inventory/WorldItem.cs
@@ -4,4 +4,29 @@ public partial class WorldItem : Node2D
 {
     [Export]
     public Item Item;
+
+    /**
+    * Scene nodes
+    */
+    private Sprite2D _sprite;
+
+    public override void _Ready()
+    {
+        InitialiseSceneNodes();
+    }
+
+    public void Highlight()
+    {
+        _sprite.Modulate = new Color(203/255.0f, 213/255.0f, 225/255.0f);
+    }
+
+    public void Unhighlight()
+    {
+        _sprite.Modulate = new Color(15/255.0f, 23/255.0f, 42/255.0f);
+    }
+
+    private void InitialiseSceneNodes()
+    {
+        _sprite = GetNode<Sprite2D>("Sprite");
+    }
 }
