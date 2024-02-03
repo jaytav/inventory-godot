b7bc80db573781b3f3439545c70eb2d4ae775170
Jayden Tavares <jayden.tavares@hotmail.com>
2024-02-03 12:37:14 +1100

    remove ItemSlotGroup, add empty World, WorldInventory, WorldItem and WorldItemSlot scenes

--- src/inventory/ItemSlotGroup.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using Godot.Collections;
-
-public partial class ItemSlotGroup : Resource
-{
-    [Export]
-    public Dictionary<Vector2, ItemSlot> ItemSlots = new();
-}
---
+++ src/scenes/world/inventory/WorldItem.cs
@@ -0,0 +1,7 @@
+using Godot;
+
+public partial class WorldItem : Node2D
+{
+    [Export]
+    public Item Item;
+}
